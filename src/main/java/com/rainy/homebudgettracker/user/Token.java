064518b8713a0d7158692f744deb35fea51ba2db
MateuszMasternak <masternak.mateusz1@gmail.com>
2024-05-05 21:09:38 +0200

    Add User and Token entities and repositories

---
+++ src/main/java/com/rainy/homebudgettracker/user/Token.java
@@ -0,0 +1,25 @@
+package com.rainy.homebudgettracker.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime confirmedAt;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
