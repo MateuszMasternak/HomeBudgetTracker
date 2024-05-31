f3914b07015a483f64829ceb4c5733dd6e0cad93
MateuszMasternak <masternak.mateusz1@gmail.com>
2024-05-31 16:10:05 +0200

    Add category pagination

--- src/main/java/com/rainy/homebudgettracker/category/CategoryRepository.java
+++ src/main/java/com/rainy/homebudgettracker/category/CategoryRepository.java
@@ -1,11 +1,14 @@
 package com.rainy.homebudgettracker.category;
 
 import com.rainy.homebudgettracker.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CategoryRepository extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByUserAndName(User user, String name);
-    Iterable<Category> findAllByUser(User user);
+    Page<Category> findAllByUser(User user, Pageable pageable);
 }
--- src/main/java/com/rainy/homebudgettracker/transaction/TransactionService.java
+++ src/main/java/com/rainy/homebudgettracker/transaction/TransactionService.java
@@ -12,8 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
