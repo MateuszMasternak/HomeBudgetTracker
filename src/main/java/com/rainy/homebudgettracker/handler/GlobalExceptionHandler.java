7ecf7d4afdcd1e1a8e412262a2d467f839f6eac4
MateuszMasternak <masternak.mateusz1@gmail.com>
2024-06-20 22:41:40 +0200

    Add new port for frontend in application-dev.yml

--- src/main/java/com/rainy/homebudgettracker/handler/GlobalExceptionHandler.java
+++ src/main/java/com/rainy/homebudgettracker/handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.rainy.homebudgettracker.handler;
 
 import com.rainy.homebudgettracker.handler.exception.*;
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.mail.MessagingException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
