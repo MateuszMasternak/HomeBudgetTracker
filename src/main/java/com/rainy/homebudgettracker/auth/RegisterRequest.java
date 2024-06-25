e835b910f0a85810d97d79bcf69d56f458693c50
MateuszMasternak <masternak.mateusz1@gmail.com>
2024-06-25 15:35:35 +0200

    Add more accepted special signs for password validation

--- src/main/java/com/rainy/homebudgettracker/auth/RegisterRequest.java
+++ src/main/java/com/rainy/homebudgettracker/auth/RegisterRequest.java
@@ -17,6 +17,6 @@ public class RegisterRequest {
     private String email;
     @NotEmpty(message = "Password is required")
     @NotBlank(message = "Password is required")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@|$!%*?&^+=<>_()/:;'\"`\\\\~-])[A-Za-z\\d@|$!%*?&^+=<>_()/:;'\"`\\\\~-]{8,}$", message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character: @|$!%*?&^+=<>_()/:;'\"`\\~-")
     private String password;
 }
