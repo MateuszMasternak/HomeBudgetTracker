03b8a8e9845a7a5c0e3c405c94b805e5ce399c1f
MateuszMasternak <masternak.mateusz1@gmail.com>
2024-08-14 11:39:17 +0200

    Delete usuned code from UserRepository

--- src/main/java/com/rainy/homebudgettracker/user/UserRepository.java
+++ src/main/java/com/rainy/homebudgettracker/user/UserRepository.java
@@ -1,15 +1,10 @@
 package com.rainy.homebudgettracker.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    @Modifying()
-    @Query("UPDATE User u SET u.password = :password WHERE u = :user")
-    void updatePassword(User user, String password);
 }
