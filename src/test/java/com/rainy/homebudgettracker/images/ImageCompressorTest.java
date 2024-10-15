fd1d0d4e38b2f76f8d69f8ea2bd36f8671b62845
MateuszMasternak <masternak.mateusz1@gmail.com>
2024-10-15 18:24:59 +0200

    Add unit tests for ImageCompressor

---
+++ src/test/java/com/rainy/homebudgettracker/images/ImageCompressorTest.java
@@ -0,0 +1,21 @@
+package com.rainy.homebudgettracker.images;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageCompressorTest {
+
+    @Test
+    void compressImage() throws IOException {
+        File inputFile = new File("src/test/resources/test.png");
+        File outputFile = ImageCompressor.compressImage(inputFile);
+
+        assertTrue(outputFile.exists());
+        assertTrue(outputFile.length() < inputFile.length());
+        assertTrue(outputFile.getName().endsWith(".jpg"));
+    }
+}
