7f494eceb667b7cde57d90d89a55b0d5a9c59634
MateuszMasternak <masternak.mateusz1@gmail.com>
2025-08-02 11:21:47 +0200

    Move caffeine params to config file

--- src/main/java/com/rainy/homebudgettracker/config/CacheConfig.java
+++ src/main/java/com/rainy/homebudgettracker/config/CacheConfig.java
@@ -1,10 +1,32 @@
 package com.rainy.homebudgettracker.config;
 
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    @Bean
+    @Primary
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("exchangeRates");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(10)
+                .maximumSize(500)
+                .expireAfterWrite(1, TimeUnit.DAYS)
+                .recordStats();
+    }
 }
